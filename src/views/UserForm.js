cda6a88d08a5b24ce3c1c795582761400f62a53a
caiosergg <caiosergio123@gmail.com>
2025-02-22 23:47:46 -0300

    feat: implementa navegação e passagem de parâmetros entre telas

--- src/views/UserForm.js
+++ src/views/UserForm.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import { Text } from "react-native";
 
-export default (props) => {
-  return <Text>UserForm</Text>;
+export default ({ route }) => {
+  const [user, setUser] = useState(route.params ? route.params : {});
+  return <Text>{user.id}</Text>;
 };
