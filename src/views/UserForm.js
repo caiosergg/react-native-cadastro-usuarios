5aa4e6f07a8fee3136590843b9c803c0b8848856
caiosergg <caiosergio123@gmail.com>
2025-02-23 01:23:12 -0300

    feat: cria formulário de cadastro de usuário

--- src/views/UserForm.js
+++ src/views/UserForm.js
@@ -1,7 +1,50 @@
 import React, { useState } from "react";
-import { Text } from "react-native";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
-export default ({ route }) => {
+export default ({ route, navigation }) => {
   const [user, setUser] = useState(route.params ? route.params : {});
-  return <Text>{user.id}</Text>;
+  return (
+    <View style={style.form}>
+      <Text>Nome</Text>
+      <TextInput
+        style={style.input}
+        onChangeText={(name) => setUser({ ...user, name })}
+        placeholder="Informe o Nome"
+        value={user.name}
+      />
+      <Text>Email</Text>
+      <TextInput
+        style={style.input}
+        onChangeText={(email) => setUser({ ...user, email })}
+        placeholder="Informe o Email"
+        value={user.email}
+      />
+      <Text>URL do Avatar</Text>
+      <TextInput
+        style={style.input}
+        onChangeText={(avatarUrl) => setUser({ ...user, avatarUrl })}
+        placeholder="Informe a URL do Avatar"
+        value={user.avatarUrl}
+      />
+      <Button
+        title="Salvar"
+        onPress={() => {
+          navigation.goBack();
+        }}
+      />
+    </View>
+  );
 };
+
+const style = StyleSheet.create({
+  form: {
+    padding: 12,
+  },
+  input: {
+    padding: 10,
+    height: 40,
+    borderColor: "gray",
+    borderWidth: 1,
+    marginBottom: 15,
+  },
+});
